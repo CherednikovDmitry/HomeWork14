0e9499a35ad034f5cbc048ba5bfc1831a03029b4
Dmitry Cherednikov <art.d1ma@ya.ru>
2022-04-10 15:53:40 +0300

    Add project files.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str = "SkillBox Homework";
+
+	std::cout << "SkillBox Homework" "\n";
+		
+	std::cout << "String Length = " << str.length();
+
+	std::cout << "The first letter = " << str.front();
+
+	std::cout << "The last letter = " << str.back();
+
+	return 0;
+}
